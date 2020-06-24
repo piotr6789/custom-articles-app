aad23403e1b84e6eeeaa6bec1bb7b7cecbf87a53
Piotr <piotr.ulanicki4@gmail.com>
2020-06-24 12:44:10 +0200

    added basic folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function App() {
-  return <div></div>;
-}
+import Layout from './hoc/Layout/Layout';
+
+const App = () => {
+  return (
+    <Layout>
+      <h1>My app</h1>
+    </Layout>
+  );
+};
 
 export default App;
---
+++ client/src/hoc/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import classes from './Layout.module.css';
+
+const Layout = (props) => {
+  return (
+    <Fragment>
+      <main className={classes.Content}>{props.children}</main>
+    </Fragment>
+  );
+};
+
+export default Layout;
